f9d2910024967a11acc0fd3ec58e0f83dd2a94e4
Jakub Ostapczuk <jakub.ostapczuk.zg@gmail.com>
2019-03-24 21:24:26 +0100

    Set up the basics

---
+++ Nidsc.java
@@ -0,0 +1,6 @@
+public class Nidsc {
+  public static void main(String args[]) {
+    System.out.println("NIDSC2 - PROJEKT");
+    System.out.println("Jakub Ostapczuk, Adam Krizar, Paweł Norberciak");
+  }
+}
